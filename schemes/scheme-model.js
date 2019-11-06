cae95b52566c80d95d12c2cfe0fc2bf32589a0c1
ericbanker12 <ericbanker12@gmail.com>
2019-11-06 15:46:59 -0600

    add findSteps to scheme-model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,9 +8,9 @@ module.exports = {
     find,
     findById,
     findSteps,
-    add,
-    update,
-    remove,
+    // add,
+    // update,
+    // remove,
 }
 
 function find() {
@@ -23,3 +23,17 @@ function findById(id) {
     return db('schemes').where({ id }).first()
 }
 
+function findSteps(id) {
+    // Resolves to an array of all correctly ordered step for the given scheme
+    /*
+        select steps.id, steps.step_number, steps.instructions, schemes.scheme_name from steps 
+        join schemes on steps.scheme_id = schemes.id 
+        where steps.scheme_id = 1 
+        order by steps.step_number;
+    */
+    return db.select('steps.id', 'steps.step_number', 'steps.instructions', 'schemes.scheme_name')
+    .from('steps')
+    .join('schemes', 'steps.scheme_id', 'schemes.id')
+    .where('steps.scheme_id', id)
+    .orderBy('steps.step_number')
+}
